36c1fa5ba74612c0963684a8f7f6ecbc95230539
Ubuntu <ubuntu@ip-172-31-21-177.ap-southeast-2.compute.internal>
2026-02-04 15:16:04 +0000

    Added Script folder

---
+++ scripts/first.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Hello my name is Saumitra"
+
---
+++ scripts/second.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+
+<< comment
+In this file we have get the input from the user and then display int on the screen.
+comment
+
+
+echo "Please enter you name!"
+
+read name
+
+echo "You have entered $name!!!"
