a36ae7e1eb7b37c2be25879c166787475a593660
Saumitra Rajput <saumitrarajput3jan@gmail.com>
2026-02-12 14:04:54 +0000

    Added more scripts

--- scripts/arguments.sh
+++ scripts/arguments.sh
@@ -1,10 +1,11 @@
 #!/etc/bash
-#
-#
+
 #this file will have info about arguments
-#
 
-
+echo "When entering argument is has to be space separated."
+echo "0th argument is filename: $0"
 echo "you have entered : $1 $2 $3 $4 $5"
-
-echo "and these are the arugments were passed into the script while executing"
+echo "1st argument: $1"
+echo "2nd argument: $2"
+echo "3rd argument: $3"
+echo "All the entered arguments are : $@" 
---
+++ scripts/liveclass/install_via_argument.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt install "$1" -y
